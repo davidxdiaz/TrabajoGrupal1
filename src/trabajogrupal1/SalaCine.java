c42615e592406220c21cb62527c552ba58752bbe
davidxdiaz <davidxdiaz@unitec.edu>
2016-10-21 07:50:52 -0600

    SalaCine

---
+++ src/trabajogrupal1/SalaCine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogrupal1;
+
+/**
+ *
+ * @author David
+ */
+public class SalaCine {
+    Ticket asientos[][]= new Ticket[10][30];
+    Coordenada buscarTicket(int numero){
+        
+    }
+    
+}
