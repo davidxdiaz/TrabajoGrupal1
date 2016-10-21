0ce30827654f7fd06135e067558f8d248267ef8c
AndyEso <andyescobar007@unitec.edu>
2016-10-21 07:52:04 -0600

    Ticket2

--- src/trabajogrupal1/Ticket.java
+++ src/trabajogrupal1/Ticket.java
@@ -13,7 +13,35 @@ import java.util.Calendar;
  */
 public class Ticket {
     int idTicket;
-    Calendar fecha=Calendar.getInstance();
+    Calendar fecha;
     double precio;
     
+    public Ticket(int n,double precio){
+        Calendar hoy=Calendar.getInstance();
+        idTicket=n;
+        this.precio=precio;
+        fecha=Calendar.getInstance();
+    }
+    
+    
+    public void println()
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    
+    
+        
+        
+        
+        
+    }
 }
