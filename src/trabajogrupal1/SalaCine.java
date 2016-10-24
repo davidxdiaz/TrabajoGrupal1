2d095398f8238661df8ce397b215370255fc839d
miguelhasbun <miguelneptaly@gmail.com>
2016-10-24 10:58:18 -0600

    Commit1

--- src/trabajogrupal1/SalaCine.java
+++ src/trabajogrupal1/SalaCine.java
@@ -10,5 +10,15 @@ package trabajogrupal1;
  * @author Miguel Paz
  */
 public class SalaCine {
-    
+    private static boolean IsPalindromo (String name, int inicio, int fin){
+       if(inicio < fin){
+            if(name.charAt(inicio) == name.charAt(fin))
+                return IsPalindromo(name,inicio+1,fin-1);
+            return false;
+        }
+        return true;
+    }
+    public static boolean IsPalindromo (String name){
+        return IsPalindromo(name,0, name.length()-1);
+    }
 }
